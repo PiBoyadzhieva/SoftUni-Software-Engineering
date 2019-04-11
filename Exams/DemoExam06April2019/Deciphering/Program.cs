31cc9d53b5cdf1071c8ba08af2ca9fa6cf53f981
Petia Boyadzhieva <pikoleva@gmail.com>
2019-04-11 13:01:33 +0300

    Added Demo Exam 06.04.2019

---
+++ Exams/DemoExam06April2019/Deciphering/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Deciphering
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+            string newString = string.Empty;
+
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                char currentChar = (char)(firstString[i] - 3);
+                newString += currentChar;
+            }
+
+            //Console.WriteLine(newString);
+            for (int i = 0; i < newString.Length; i++)
+            {
+                bool isCorect = (newString[i] >= 97 && newString[i] <= 125) || newString[i] == '#' || newString[i] == ' ';
+
+                if (isCorect == false)
+                {
+                    Console.WriteLine($"This is not the book you are looking for.");
+                    return;
+                }
+
+            }
+
+            for (int i = 0; i < newString.Length; i++)
+            {
+                string[] splitedSecondString = secondString.Split(" ");
+                string firstSubstring = splitedSecondString[0];
+                string secondSubstring = splitedSecondString[1];
+
+                if (newString.Contains(firstSubstring))
+                {
+                    newString = newString.Replace(firstSubstring, secondSubstring);
+                }
+            }
+
+            Console.WriteLine(newString);
+        }
+    }
+}
