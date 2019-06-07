d786297f1c0cd9c243e40b2713bf1d679df12979
PiBoyadzhieva <pikoleva@gmail.com>
2019-06-07 23:50:02 +0300

    Added Defining classes Lab

---
+++ C#Advanced/DefiningClassesLab/Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        //private int year;
+        //private int model;
+        //private string make;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ C#Advanced/DefiningClassesLab/CarConstructors/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Car myCar = new Car();
+
+            myCar.Make = "BMW";
+            myCar.Model = "M5 Competition";
+            myCar.Year = 2018;
+            myCar.FuelQuantity = 54;
+            myCar.FuelConsumption = 12;
+
+            Console.WriteLine(myCar.WhoAmI());
+
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsuption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsuption);
+
+            //Console.WriteLine(car.WhoAmI());
+
+        }
+    }
+}
