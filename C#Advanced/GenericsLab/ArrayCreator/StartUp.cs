5bdf48271337658af77193c4f7de23985d904ac9
PiBoyadzhieva <pikoleva@gmail.com>
2019-06-13 14:54:20 +0300

    Generics Lib Done

---
+++ C#Advanced/GenericsLab/ArrayCreator/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        public static void Main()
+        {
+            //string[] strings = ArrayCreator<string>.Create(5, "Pesho");
+
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+
+            int[] integers = ArrayCreator.Create(10, 33);
+        }
+    }
+}
