09bd51c44123f7b13f29666e83efb9e553950b04
PiBoyadzhieva <pikoleva@gmail.com>
2019-06-18 19:16:15 +0300

    Exam 17 Feb Done

---
+++ AdvancedExams/AdvancedExam17Feb2019/ExcelFunctions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelFunctions
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            var jaggedArr = new string[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] currentRow = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                jaggedArr[row] = currentRow;
+            }
+
+            string[] command = Console.ReadLine()
+                .Split();
+            
+            string header = command[1];
+            int targetIndex = Array.IndexOf(jaggedArr[0], header);
+
+            if (command[0] == "hide")
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int row = 0; row < jaggedArr.GetLength(0); row++)
+                {
+                    List<string> line = new List<string>();
+
+                    for (int col = 0; col < jaggedArr[0].Length; col++)
+                    {
+                        if (col != targetIndex)
+                        {
+                            line.Add(jaggedArr[row][col]);
+                        }
+                    }
+
+                    sb.AppendLine(string.Join(" | ", line));
+                }
+
+                Console.WriteLine(sb.ToString().TrimEnd());
+            }
+
+            else if (command[0] == "sort")
+            {
+
+                Console.WriteLine(string.Join(" | ", jaggedArr[0]));
+
+                foreach (var row in jaggedArr.Skip(1).OrderBy(r => r[targetIndex]))
+                {
+                    Console.WriteLine($"{string.Join(" | ", row)}");
+                }
+            }
+
+            else if (command[0] == "filter")
+            {
+                string value = command[2];
+
+                Console.WriteLine(string.Join(" | ", jaggedArr[0]));
+
+                for (int row = 0; row < rows; row++)
+                {
+                    if (jaggedArr[row][targetIndex] == value)
+                    {
+                        Console.WriteLine(string.Join(" | ", jaggedArr[row]));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AdvancedExams/AdvancedExam17Feb2019/Socks/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Socks
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] inputLeftSocks = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] inputRightSocks = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> leftSocks = new Stack<int>(inputLeftSocks);
+            Stack<int> rightSocks = new Stack<int>(inputRightSocks.Reverse());
+
+            List<int> pair = new List<int>();
+
+            while (leftSocks.Any() && rightSocks.Any())
+            {
+                int currentLeftSock = leftSocks.Pop();
+                int currentRightSock = rightSocks.Pop();
+
+                if(currentLeftSock > currentRightSock)
+                {
+                    pair.Add(currentLeftSock + currentRightSock);
+                }
+                else if(currentRightSock > currentLeftSock)
+                {
+                    rightSocks.Push(currentRightSock);
+                    continue;
+                }
+
+                else if(currentLeftSock == currentRightSock)
+                {
+                    currentLeftSock += 1;
+                    leftSocks.Push(currentLeftSock);
+                    continue;
+                }
+            }
+
+            Console.WriteLine(pair.Max());
+            Console.WriteLine(string.Join(" ", pair));
+        }
+    }
+}
