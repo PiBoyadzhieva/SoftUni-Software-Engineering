7508467f507d9c69ce71c48e74aa80d134ca3ed0
PiBoyadzhieva <pikoleva@gmail.com>
2019-07-12 00:44:45 +0300

    Added Interfaces and Abstraction Lab

---
+++ C# OOP/InterfacesAndAbstractionLab/Shapes/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private readonly int width;
+        private readonly int height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public void Draw()
+        {
+            DrawLine('*', '*');
+
+            for (int i = 0; i < this.height - 2; i++)
+            {
+                DrawLine('*', ' ');
+            }
+
+            DrawLine('*', '*');
+        }
+
+        private void DrawLine(char borderSymbol, char fillerSymbol)
+        {
+            Console.Write(borderSymbol);
+
+            for (int i = 0; i < this.width - 2; i++)
+            {
+                Console.Write(fillerSymbol);
+            }
+
+            Console.WriteLine(borderSymbol);
+        }
+    }
+}
