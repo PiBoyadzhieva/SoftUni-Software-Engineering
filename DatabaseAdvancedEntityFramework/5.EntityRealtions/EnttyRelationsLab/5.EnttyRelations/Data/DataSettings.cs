108aee22a1d465941b3c78816ffcf2946c7774a2
PiBoyadzhieva <pikoleva@gmail.com>
2019-11-10 00:50:36 +0200

    Added Entity Relations Lab - demo

---
+++ DatabaseAdvancedEntityFramework/5.EntityRealtions/EnttyRelationsLab/5.EnttyRelations/Data/DataSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace _5.EnttyRelations.Data
+{
+    public class DataSettings
+    {
+        public const string DefaultConnection = @"Server=.\SQLEXPRESS01;Database=CarSystem;Integrated Security=True";
+    }
+}
---
+++ DatabaseAdvancedEntityFramework/5.EntityRealtions/EnttyRelationsLab/5.EnttyRelations/Results/CustomerResultModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _5.EnttyRelations.Results
+{
+    public class CustomerResultModel
+    {
+        public string Name { get; set; }
+
+        public string Town { get; set; }
+    }
+}
