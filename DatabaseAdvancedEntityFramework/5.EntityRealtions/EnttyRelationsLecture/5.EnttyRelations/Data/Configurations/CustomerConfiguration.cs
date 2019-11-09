1a5ff966ac82b12d4b090045382cee1d95c72e9d
PiBoyadzhieva <pikoleva@gmail.com>
2019-11-10 00:59:36 +0200

    Renamed folder

---
+++ DatabaseAdvancedEntityFramework/5.EntityRealtions/EnttyRelationsLecture/5.EnttyRelations/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace _5.EnttyRelations.Data.Configurations
+{
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> customer)
+        {
+            customer
+                    .HasOne(c => c.Address)
+                    .WithOne(a => a.Customer)
+                    .HasForeignKey<Address>(a => a.CustomerId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ DatabaseAdvancedEntityFramework/5.EntityRealtions/EnttyRelationsLecture/5.EnttyRelations/Results/PurchaseResultModel.cs
@@ -0,0 +1,15 @@
+﻿namespace _5.EnttyRelations.Results
+{
+    using System;
+
+    public class PurchaseResultModel
+    {
+        public decimal Price { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public CarResultModel Car { get; set; }
+
+        public CustomerResultModel Customer { get; set; }
+    }
+}
