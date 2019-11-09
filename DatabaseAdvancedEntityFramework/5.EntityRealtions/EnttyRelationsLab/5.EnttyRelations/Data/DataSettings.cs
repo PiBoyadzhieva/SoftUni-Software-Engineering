78d631e30ddc3f1326505d63dcd1f88e93cf6494
PiBoyadzhieva <pikoleva@gmail.com>
2019-11-10 00:58:53 +0200

    Renamed folder

--- DatabaseAdvancedEntityFramework/5.EntityRealtions/EnttyRelationsLab/5.EnttyRelations/Data/DataSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _5.EnttyRelations.Data
-{
-    public class DataSettings
-    {
-        public const string DefaultConnection = @"Server=.\SQLEXPRESS01;Database=CarSystem;Integrated Security=True";
-    }
-}
